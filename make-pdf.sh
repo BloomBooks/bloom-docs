7ffe1109962a27b947e4bc391658b6ddd469748a
Andrew Polk <andrew_polk@sil.org>
2023-09-12 15:27:16 -0700

    Test run of new pdf creation process

--- make-pdf.sh
+++ make-pdf.sh
@@ -1,19 +1,54 @@
 #!/usr/bin/env bash
 
-# removed becuase of https://github.com/kohheepeace/mr-pdf/issues/60
+# Use en for the language if not set
+if [ -z "$1" ]
+then
+  set -- 'en' "$@"
+fi
+
+if [ "$1" == "en" ]; then
+    URL="https://docs.bloomlibrary.org"
+else
+    URL="https://docs.bloomlibrary.org/$1"
+fi
+
+LANGUAGENAME="english"
+if [ $1 == "fr" ]; then
+    LANGUAGENAME="french"
+elif [ $1 == "es" ]; then
+    LANGUAGENAME="spanish"
+fi
+
+# By generating the PDFs directly in the build (not static) dir, we can avoid having to build the
+# docusaurus site all over again in the workflow.
+# Also, if we generate the PDFs in the static folder,
+# the docusaurus build copies all of them to each locale by default, so we get duplicates.
+if [ "$1" == "en" ]; then
+    mkdir -p "build/downloads"
+    OUTPUTPATH="build/downloads/docs-bloomlibrary-$LANGUAGENAME-a4.pdf"
+else
+    mkdir -p "build/$1/downloads"
+    OUTPUTPATH="build/$1/downloads/docs-bloomlibrary-$LANGUAGENAME-a4.pdf"
+fi
+
+# removed because of https://github.com/kohheepeace/mr-pdf/issues/60
 # --coverImage="https://docs.bloomlibrary.org/pdf-cover.png" \
 
-today=$(date +"%Y-%m-%d")
-
-npx docu-pdf https://docs.bloomlibrary.org \
+npx docu-pdf "$URL" \
 --coverPath "./pdf-cover.htm" \
 --pageSize "A4" \
---outputPath "static/downloads/docs-bloomlibrary-english-a4.pdf" \
+--outputPath "$OUTPUTPATH" \
 --tocLevel 1
+
+if [ "$1" == "en" ]; then
+    # Until we start generating language-specific PDFs, we'll just copy the English one to the other locales.
+    cp "$OUTPUTPATH" "build/fr/downloads/docs-bloomlibrary-english-a4.pdf"
+    cp "$OUTPUTPATH" "build/es/downloads/docs-bloomlibrary-english-a4.pdf"
+fi
 
 # npx docu-pdf https://docs.bloomlibrary.org/fr \
 # --coverTitle "Docs Bloom" \
 # --coverSub="Créé à partir de docs.bloomlibrary.org le ${today}" \
 # --pageSize "A4" \
-# --outputPath "static/downloads/docs-bloomlibrary-french-a4.pdf" \
+# --outputPath "build/downloads/docs-bloomlibrary-french-a4.pdf" \
 
